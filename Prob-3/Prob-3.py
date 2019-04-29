72f913add205c9dc6982f4f798ce0083d5c2b783
rachel watson <r.watson7@students.clark.edu>
2019-04-29 15:47:25 -0700

    Problem 3 assignment 2

--- Prob-3/Prob-3.py
+++ Prob-3/Prob-3.py
@@ -2,7 +2,7 @@
 #   Programming Assignment 2
 #     Prob-3.py
 
-# YOUR NAME
+# Rachel Watson
 
 def example():
     print("\nExample Output")
@@ -21,22 +21,33 @@ def example():
 
 def studentCode():
     # replace <name> with your name
-    print("\n<name>'s Output")
+    print("\nRachel's Output")
     
     # print a blank line
     print()
 
     # replicate the assignment statement above, but use your own variable
+    
     # names and values
-
+    v1, v2, v3 = 3.14, 6485, "CTEC 121"
     # print the values of the 3 variables
-
+    print("myv1:", v1)
+    print("myv2:", v2)
+    print("myv3:", v3)
     # Get 3 values from the user and assign them to the variables defined
     # above. See the page in Canvas on Simulataneous Assignment
+    print()
+
+    v1, v2, v3 = input("Enter 3 values separated by commas:"). split()
+    print("myv1:", v1)
+    print("myv2:", v2)
+    print("myv3:", v3)
+
     # BONUS POINTS for using the split() method
 
     print()
 
 example()
 studentCode()
+
 
